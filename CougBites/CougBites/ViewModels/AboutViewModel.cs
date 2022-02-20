7e44f50a6ceb11f3ebccc664b62704bde67a300a
Aiden41 <aiden41dickson@outlook.com>
2022-02-19 18:07:02 -0800

    pushing template app

--- CougBites/CougBites/ViewModels/AboutViewModel.cs
+++ CougBites/CougBites/ViewModels/AboutViewModel.cs
@@ -9,7 +9,7 @@ namespace CougBites.ViewModels
     {
         public AboutViewModel()
         {
-            Title = "About";
+            Title = "Home";
             OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
         }
 
--- CougBites/CougBites/ViewModels/SearchViewModel.cs
+++ CougBites/CougBites/ViewModels/SearchViewModel.cs
@@ -8,7 +8,7 @@ using Xamarin.Forms;
 
 namespace CougBites.ViewModels
 {
-    public class ItemsViewModel : BaseViewModel
+    public class SearchViewModel : BaseViewModel
     {
         private Item _selectedItem;
 
@@ -17,9 +17,9 @@ namespace CougBites.ViewModels
         public Command AddItemCommand { get; }
         public Command<Item> ItemTapped { get; }
 
-        public ItemsViewModel()
+        public SearchViewModel()
         {
-            Title = "Meal Planner";
+            Title = "Search";
             Items = new ObservableCollection<Item>();
             LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
