2f602fbf84484fcaf55bc25e2c8a12405de0c597
Ben Kaufmann <benk01@hotmail.com>
2022-02-19 18:07:32 -0800

    started db

---
+++ CougBites/CougBites/Models/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CougBites.Models
+{
+    public class Location
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        //Picture
+        //Hours of Operation
+        public FoodItem[] FoodItems { get; set; }
+        public string Menu { get; set; }
+    }
+}
---
+++ CougBites/CougBites/Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CougBites.Models
+{
+    public class Profile
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //Picture
+        public EnvironmentVariableTarget 
+    }
+}
