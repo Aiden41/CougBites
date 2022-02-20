2283eeb546ff5629f9547b71ea18822d3f573aa6
Ben Kaufmann <benk01@hotmail.com>
2022-02-20 08:24:42 -0800

    poop

--- CougBites/CougBites/ViewModels/AboutViewModel.cs
+++ CougBites/CougBites/ViewModels/AboutViewModel.cs
@@ -8,7 +8,7 @@ using System.IO;
 namespace CougBites.ViewModels
 {
     public class AboutViewModel : BaseViewModel
-    {
+    { 
         public AboutViewModel()
         {
             Title = "Home";
@@ -16,6 +16,7 @@ namespace CougBites.ViewModels
             Database database = new Database(Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "MyDB.db3"));
             var forYou = database._database.Table<Models.Rating>().Where(v => v.UserId == 5 && v.RatingNum > 3.5);
             var forEveryone = database._database.Table<Models.Rating>().Where(v => v.UserId != 5 && v.RatingNum > 3.5);
+            //
         }
 
         public ICommand OpenWebCommand { get; }
